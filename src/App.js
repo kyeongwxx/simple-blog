d7386334244253a89d49b545e6d69e8c44339afb
kyeongwxx <kyeongwxx@gmail.com>
2022-12-04 11:59:21 +0900

    feat: 레이아웃 구현
    블로그 로고, 글 목록 생성
    글 목록의 title은 state에 저장하여 html에 데이터바인

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  let logo = "SimpleBlog"
+
+  let [title1, setTitle2] = useState('모던 자바스크립트 DEEP DIVE')
+  let [title2, setTitle1] = useState('리액트를 다루는 기술')
+  let [title3, setTitle3] = useState('알고리즘 첫걸음')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="nav">
+        <h4 className="logo">{logo}</h4>
+      </div>
+      <div className="list">
+        <h4>{title1}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title2}</h4>
+        <p>2월 18일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title3}</h4>
+        <p>2월 19일 발행</p>
+      </div>
     </div>
   );
 }
