a0e315ebe473b2b3560db3d7fb5048dd8f94049b
kyeongwxx <kyeongwxx@gmail.com>
2022-12-04 13:47:17 +0900

    feat: 좋아요 기능 구현
    title 옆 좋아요 아이콘 생성 및 클릭 시 count + 1
    state 변경 함수 사용

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  let logo = "SimpleBlog"
+  let logo = "SimpleBlog";
 
-  let [title1, setTitle2] = useState('모던 자바스크립트 DEEP DIVE')
-  let [title2, setTitle1] = useState('리액트를 다루는 기술')
-  let [title3, setTitle3] = useState('알고리즘 첫걸음')
+  let [title1, setTitle1] = useState("모던 자바스크립트 DEEP DIVE");
+  let [title2, setTitle2] = useState("리액트를 다루는 기술");
+  let [title3, setTitle3] = useState("알고리즘 첫걸음");
+
+  let [like, setLike] = useState(0);
+  const onClickLike = () => {
+    setLike(like + 1);
+  };
+
+  const onClickChangeTitle = () => {
+    setTitle1("모던 자바스크립트 SHALLOW DIVE");
+  };
 
   return (
     <div>
@@ -14,7 +23,10 @@ function App() {
         <h4 className="logo">{logo}</h4>
       </div>
       <div className="list">
-        <h4>{title1}</h4>
+        <h4 onClick={onClickChangeTitle}>
+          {title1} <span onClick={onClickLike}>👍</span>{" "}
+          <span className="like">{like}</span>
+        </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
