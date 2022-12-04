4469301248ac8cd78fa47aef06ad4edee87007f3
kyeongwxx <kyeongwxx@gmail.com>
2022-12-04 23:11:24 +0900

    feat: 모달 컴포넌트 구현
    title 클릭 시 나타나는 모달 컴포넌트 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import { useState } from "react";
 import "./App.css";
 
+const Modal = () => {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  );
+};
+
 function App() {
   let logo = "SimpleBlog";
 
@@ -9,11 +19,6 @@ function App() {
     "리액트를 다루는 기술",
     "알고리즘 첫걸음",
   ]);
-
-  let [like, setLike] = useState(0);
-  const onClickLike = () => {
-    setLike(like + 1);
-  };
 
   const onClickChangeTitle = () => {
     let copy = [...title];
@@ -26,12 +31,24 @@ function App() {
     setTitle(copy);
   };
 
+  let [like, setLike] = useState(0);
+
+  const onClickLike = () => {
+    setLike(like + 1);
+  };
+
+  let [modalOpen, setModalOpen] = useState(false);
+
+  const onClickModal = () => {
+    setModalOpen((prev) => !prev);
+  };
+
   return (
     <div>
       <div className="nav">
         <h4 className="logo">{logo}</h4>
       </div>
-      
+
       <div className="btns">
         <button className="title-change-btn" onClick={onClickChangeTitle}>
           title 변경
@@ -53,9 +70,11 @@ function App() {
         <p>2월 18일 발행</p>
       </div>
       <div className="list">
-        <h4>{title[2]}</h4>
+        <h4 onClick={onClickModal}>{title[2]}</h4>
         <p>2월 19일 발행</p>
       </div>
+
+      {modalOpen && <Modal />}
     </div>
   );
 }
